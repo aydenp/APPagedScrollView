80b04833121a0ebe0ea671cbf3648a511cdf712c
Ayden P <aydenp123+gh@icloud.com>
2017-07-07 17:27:26 -0400

    Initial Commit

---
+++ APPagedScrollView/APPagedScrollView.h
@@ -0,0 +1,19 @@
+//
+//  APPagedScrollView.h
+//  APPagedScrollView
+//
+//  Created by Ayden P on 2017-07-07.
+//  Copyright © 2017 Ayden P. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APPagedScrollView.
+FOUNDATION_EXPORT double APPagedScrollViewVersionNumber;
+
+//! Project version string for APPagedScrollView.
+FOUNDATION_EXPORT const unsigned char APPagedScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APPagedScrollView/PublicHeader.h>
+
+
